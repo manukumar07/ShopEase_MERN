d588228e54f07864475b944e776b9be20cae093f
Manu Kumar Pal <manukumarpal34@gmail.com>
2024-04-20 16:50:39 +0530

    add routes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDb = require("./config/db");
+const authRoute = require("./routes/authRoute");
 
 dotenv.config();
 
@@ -11,13 +12,16 @@ const app = express();
 
 const PORT = process.env.PORT || 5000; // define port number
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
 
-//
-app.get("/", (req, res) => {
-  res.json("hello");
-});
+app.use("/api", authRoute);
 
 connectDb();
 
